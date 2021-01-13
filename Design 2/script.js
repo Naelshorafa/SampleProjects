72565fe1a5d19a18cf6dc100d7320989bfccafba
naelshorafa.com <naelshorafa@gmail.com>
2021-01-13 11:36:08 -0800

    disign2-slider

---
+++ Design 2/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.hero-slider').bxSlider({
+    mode: 'vertical',
+    tickerHover: true,
+    controls: false,
+    auto: true,
+    pager: false,
+  });
+  $('.pic-slider').bxSlider({
+    mode: 'fade',
+    tickerHover: true,
+    controls: false,
+    auto: true,
+    pager: false,
+  });
+});
