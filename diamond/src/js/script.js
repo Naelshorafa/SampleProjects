c0eed180da1b44c2efdbd535a7c35b75a0b12411
Wael Alshurafa <wael.alshurafa@booking.com>
2021-03-17 22:14:25 +0100

    modals, datepicker, radio

--- diamond/src/js/script.js
+++ diamond/src/js/script.js
@@ -1,16 +1,33 @@
 'use strict';
-var $ = require( "jquery" );
+var $ = require("jquery");
 require('../../node_modules/bootstrap/dist/js/bootstrap.bundle');
 require('slick-carousel');
+require('bootstrap-datepicker');
 
 $(function () {
+  $.fn.datepicker.dates['ar'] = {
+    days: ["الأحد", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت", "الأحد"],
+    daysShort: ["أحد", "اثنين", "ثلاثاء", "أربعاء", "خميس", "جمعة", "سبت", "أحد"],
+    daysMin: ["ح", "ن", "ث", "ع", "خ", "ج", "س", "ح"],
+    months: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
+    monthsShort: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
+    today: "هذا اليوم",
+    rtl: true
+  };
+
   $(".success-partners .partners-slider").slick({
     slidesToShow: 9,
     slidesToScroll: 1,
     infinite: true,
     rtl: true,
-    prevArrow:'.arrow-prev',
-    nextArrow:'.arrow-next',
+    prevArrow: '.arrow-prev',
+    nextArrow: '.arrow-next',
+  });
+
+  $('.from-control-datepicker').datepicker({
+    format: "d M yyyy",
+    language: "ar",
+    autoclose: true
   });
 });
 
