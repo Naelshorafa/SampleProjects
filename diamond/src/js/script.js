d02e7bfe15791c9bfdb77ce25174c7b2c44934d2
naelshorafa.com <naelshorafa@gmail.com>
2021-03-17 17:41:16 -0700

    responsive

--- diamond/src/js/script.js
+++ diamond/src/js/script.js
@@ -1,18 +1,62 @@
-'use strict';
+"use strict";
 var $ = require("jquery");
-require('../../node_modules/bootstrap/dist/js/bootstrap.bundle');
-require('slick-carousel');
-require('bootstrap-datepicker');
+require("../../node_modules/bootstrap/dist/js/bootstrap.bundle");
+require("slick-carousel");
+require("bootstrap-datepicker");
 
 $(function () {
-  $.fn.datepicker.dates['ar'] = {
-    days: ["الأحد", "الاثنين", "الثلاثاء", "الأربعاء", "الخميس", "الجمعة", "السبت", "الأحد"],
-    daysShort: ["أحد", "اثنين", "ثلاثاء", "أربعاء", "خميس", "جمعة", "سبت", "أحد"],
+  $.fn.datepicker.dates["ar"] = {
+    days: [
+      "الأحد",
+      "الاثنين",
+      "الثلاثاء",
+      "الأربعاء",
+      "الخميس",
+      "الجمعة",
+      "السبت",
+      "الأحد",
+    ],
+    daysShort: [
+      "أحد",
+      "اثنين",
+      "ثلاثاء",
+      "أربعاء",
+      "خميس",
+      "جمعة",
+      "سبت",
+      "أحد",
+    ],
     daysMin: ["ح", "ن", "ث", "ع", "خ", "ج", "س", "ح"],
-    months: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
-    monthsShort: ["يناير", "فبراير", "مارس", "أبريل", "مايو", "يونيو", "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"],
+    months: [
+      "يناير",
+      "فبراير",
+      "مارس",
+      "أبريل",
+      "مايو",
+      "يونيو",
+      "يوليو",
+      "أغسطس",
+      "سبتمبر",
+      "أكتوبر",
+      "نوفمبر",
+      "ديسمبر",
+    ],
+    monthsShort: [
+      "يناير",
+      "فبراير",
+      "مارس",
+      "أبريل",
+      "مايو",
+      "يونيو",
+      "يوليو",
+      "أغسطس",
+      "سبتمبر",
+      "أكتوبر",
+      "نوفمبر",
+      "ديسمبر",
+    ],
     today: "هذا اليوم",
-    rtl: true
+    rtl: true,
   };
 
   $(".success-partners .partners-slider").slick({
@@ -20,14 +64,25 @@ $(function () {
     slidesToScroll: 1,
     infinite: true,
     rtl: true,
-    prevArrow: '.arrow-prev',
-    nextArrow: '.arrow-next',
+    prevArrow: ".arrow-prev",
+    nextArrow: ".arrow-next",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 5,
+        },
+      },
+    ],
   });
 
-  $('.from-control-datepicker').datepicker({
+  $(".from-control-datepicker").datepicker({
     format: "d M yyyy",
     language: "ar",
-    autoclose: true
+    autoclose: true,
   });
 });
-
