6966c681eb7eb7093ed36a339c0b96569a7e145c
naelshorafa.com <naelshorafa@gmail.com>
2021-03-21 07:29:28 -0700

    a

--- diamond/src/js/script.js
+++ diamond/src/js/script.js
@@ -95,3 +95,16 @@ $(function () {
     autoclose: true,
   });
 });
+(function ($) {  
+  $(document).ready(function(){
+  $(".ac-label").click(function(e){
+    e.preventDefault();
+    $check = $(this).prev();
+    if($check.prop('checked'))
+      $check.prop( "checked", false );
+    else 
+      $check.prop( "checked", true );
+    });
+
+  });
+})(jQuery);
