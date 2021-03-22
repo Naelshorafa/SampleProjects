635f548cbcf64a5bb76c00fa801903784b1e6dd6
Wael Alshurafa <wael.alshurafa@booking.com>
2021-03-22 23:00:17 +0100

    fixes

--- diamond/src/js/script.js
+++ diamond/src/js/script.js
@@ -69,13 +69,19 @@ $(function () {
   });
 
   $(".success-partners .partners-slider").slick({
-    slidesToShow: 9,
+    slidesToShow: 7,
     slidesToScroll: 1,
     infinite: true,
     rtl: true,
     prevArrow: ".arrow-prev",
     nextArrow: ".arrow-next",
     responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
       {
         breakpoint: 768,
         settings: {
@@ -85,15 +91,21 @@ $(function () {
       {
         breakpoint: 992,
         settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
           slidesToShow: 5,
         },
       },
       {
         breakpoint: 1400,
         settings: {
-          slidesToShow: 7,
+          slidesToShow: 6,
         },
-      },
+      },      
     ],
   });
 
